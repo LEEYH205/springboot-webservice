ae5f60317b51dce6a697b8ceb5693a55b41f9c6a
lyh-windows <ejrdkachry@gmail.com>
2020-04-13 15:08:56 +0900

    p.222

--- src/main/java/com/lyh/book/springboot/Application.java
+++ src/main/java/com/lyh/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing                //p.221에서 삭제
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
